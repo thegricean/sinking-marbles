71df4ebc6c02719468c9ca0620a598e4dea923f8
thegricean <judithdegen@gmail.com>
2015-01-28 08:08:58 -0800

    update cogsci paper

--- writing/_2015/cogsci_2015/rscripts/plots.r
+++ writing/_2015/cogsci_2015/rscripts/plots.r
@@ -172,6 +172,33 @@ library(lmerTest)
 m=lmer(normresponse ~ AllPriorProbability + (1+AllPriorProbability|workerid) + (1|Item), data=s)
 summary(m)
 
+
+
+#####################################
+# plot model predictions: wonkiness
+load("~/cogsci/projects/stanford/projects/thegricean_sinking-marbles/models/wonky_world/results/data/wr-uniform.RData")
+load("~/cogsci/projects/stanford/projects/thegricean_sinking-marbles/models/wonky_world/results/data/wr-binomial.RData")
+
+# plot expectations for best basic model: 
+toplot = droplevels(subset(wr, QUD == "how-many" & Alternatives == "0_basic" & WonkyWorldPrior == .5 & Wonky == "true"))
+nrow(toplot)
+
+toplot = droplevels(subset(toplot, SpeakerOptimality == 2))
+nrow(toplot)
+head(toplot)
+
+ggplot(toplot, aes(x=PriorExpectation, y=PosteriorProbability,color=Quantifier)) +
+  geom_point() + #color="#00B0F6") + #values=c("#F8766D", "#A3A500", "#00BF7D", "#E76BF3", "#00B0F6")
+  geom_smooth() + #color="#00B0F6") +
+  #  geom_abline(intercept=0,slope=1,color="gray50") +
+  scale_color_manual(values=c("#F8766D","#00BF7D","#00B0F6")) +
+  scale_x_continuous(breaks=seq(1,15,by=2),name="Prior expected number of objects") +
+  scale_y_continuous(breaks=seq(0,1,by=.25),name="Model predicted wonkiness probability")
+
+ggsave("model-wonkiness-uniform.pdf",width=5.5,height=4)#,width=30,height=10)
+ggsave("model-wonkiness-binomial.pdf",width=5.5,height=4)#,width=30,height=10)
+
+
 ###############
 ## EMPIRICAL PLOTS
 ###############
@@ -268,145 +295,9 @@ ggsave("noahs-allstate-predictions-bypriorexp.pdf",width=4.5,height=3.5)
 
 
 
-######### CLEAN UP REST OF THIS ##########
-
-
-
-#' get model predictions
-load("data/mp-uniform.RData")
-#d = read.table("data/parsed_uniform_results.tsv", quote="", sep="\t", header=T)
-table(d$Item)
-nrow(d)
-head(d)
-d[d$Item == "ate the seeds birds" & d$QUD=="how-many" & d$Alternatives=="0_basic" & d$SpeakerOptimality == 1,]
-d[d$Item == "stuck to the wall baseballs" & d$QUD=="how-many" & d$Alternatives=="0_basic" & d$SpeakerOptimality == 2 & d$Wonky == "true",]
-mp = ddply(d, .(Item, QUD, State, Alternatives, Quantifier, SpeakerOptimality, WonkyWorldPrior), summarise, PosteriorProbability=sum(PosteriorProbability))
-head(mp)
-#mp[mp$Item == "ate the seeds birds" & mp$QUD=="how-many" & mp$Alternatives=="0_basic" & mp$SpeakerOptimality == 1,]
-wr = ddply(d, .(Item, QUD, Wonky, Alternatives, Quantifier, SpeakerOptimality, WonkyWorldPrior), summarise, PosteriorProbability=sum(PosteriorProbability))
-wr[wr$Item == "ate the seeds birds" & wr$QUD=="how-many" & wr$Alternatives=="0_basic" & wr$SpeakerOptimality == 1,]
-
-
-
-# get smoothed prior probabilities
-priorprobs = read.table(file="~/cogsci/projects/stanford/projects/thegricean_sinking-marbles/experiments/12_sinking-marbles-prior15/results/data/smoothed_15marbles_priors_withnames.txt",sep="\t", header=T, quote="")
-head(priorprobs)
-row.names(priorprobs) = paste(priorprobs$effect,priorprobs$object)
-mpriorprobs = melt(priorprobs, id.vars=c("effect", "object"))
-head(mpriorprobs)
-row.names(mpriorprobs) = paste(mpriorprobs$effect,mpriorprobs$object,mpriorprobs$variable)
-mp$PriorProbability = mpriorprobs[paste(as.character(mp$Item)," X",mp$State,sep=""),]$value
-mp$AllPriorProbability = priorprobs[paste(as.character(mp$Item)),]$X15
-head(mp)
-
-# get empirical state posteriors:
-load("/Users/titlis/cogsci/projects/stanford/projects/thegricean_sinking-marbles/experiments/3_sinking-marbles-nullutterance/results/data/r.RData")
-head(r)
-r$Item = as.factor(paste(r$effect,r$object))
-# because posteriors come in 4 bins, make Bin variable for model prediction dataset:
-mp$Proportion = as.factor(ifelse(mp$State == 0, "0", ifelse(mp$State == 15, "100", ifelse(mp$State < 8, "1-50", "51-99"))))
-
-agr = aggregate(normresponse ~ Item + quantifier + Proportion,data=r,FUN=mean)
-#agr$CILow = aggregate(normresponse ~ Item + quantifier + Proportion,data=r, FUN=ci.low)$normresponse
-#agr$CIHigh = aggregate(normresponse ~ Item + quantifier + Proportion,data=r,FUN=ci.high)$normresponse
-#agr$YMin = agr$normresponse - agr$CILow
-#agr$YMax = agr$normresponse + agr$CIHigh
-agr$Quantifier = as.factor(tolower(agr$quantifier))
-row.names(agr) = paste(agr$Item, agr$Proportion, agr$Quantifier)
-mp$PosteriorProbability_empirical = agr[paste(mp$Item,mp$Proportion,mp$Quantifier),]$normresponse
-
-#plot empirical against predicted distributions for "some"
-some = ddply(mp, .(Item, QUD, Alternatives, Quantifier, SpeakerOptimality, PriorExpectation, Proportion, WonkyWorldPrior, PosteriorProbability_empirical), summarise, PosteriorProbability_predicted=sum(PosteriorProbability), PriorProbability_smoothed=sum(PriorProbability))
-some= subset(some, Quantifier == "some")
-nrow(some)
-head(some)
-msome = melt(some, measure.vars=c("PosteriorProbability_empirical","PosteriorProbability_predicted","PriorProbability_smoothed"))
-msome$ptype = as.factor(ifelse(msome$variable == "PosteriorProbability_empirical", "posterior (empirical)",ifelse(msome$variable == "PosteriorProbability_predicted","posterior (model)", "prior")))
-head(msome)
-nrow(msome)
-summary(msome)
-
-toplot = droplevels(subset(msome, QUD == "how-many" & SpeakerOptimality == 2 & Alternatives == "0_basic"))#"0_basic1_lownum2_extra4_twowords5_threewords"))
-nrow(toplot)
-toplot$Probability = as.factor(ifelse(toplot$ptype == "prior","prior","posterior"))
-toplot$Prop = factor(toplot$Proportion, levels=c("1-50","51-99","100"))
-ggplot(toplot, aes(x=Prop, y=value,color=ptype, group=ptype, size=Probability)) +
-  geom_point() +
-  geom_line() +
-  scale_size_discrete(range=c(1,2)) +
-  scale_color_manual(values=c("red","blue","black")) +
-  facet_wrap(WonkyWorldPrior~Item)
-ggsave("graphs/model-empirical-uniform-howmany-2-basic.pdf",width=35,height=30)
-
-#plot empirical against predicted expectations for "some"
-load("/Users/titlis/cogsci/projects/stanford/projects/thegricean_sinking-marbles/experiments/13_sinking-marbles-priordv-15/results/data/r.RData")
-summary(r)
-r$Item = as.factor(paste(r$effect, r$object))
-agr = aggregate(ProportionResponse ~ Item + quantifier, data=r, FUN=mean)
-#agr$CILow = aggregate(ProportionResponse ~ Item + quantifier,data=r, FUN=ci.low)$ProportionResponse
-#agr$CIHigh = aggregate(ProportionResponse ~ Item + quantifier,data=r,FUN=ci.high)$ProportionResponse
-#agr$YMin = agr$ProportionResponse - agr$CILow
-#agr$YMax = agr$ProportionResponse + agr$CIHigh
-agr$Quantifier = as.factor(tolower(agr$quantifier))
-row.names(agr) = paste(agr$Item, agr$Quantifier)
-mp$PosteriorExpectation_empirical = agr[paste(mp$Item,mp$Quantifier),]$ProportionResponse
-mp$PriorExpectation_smoothed = mp$PriorExpectation/15
-
-pexpectations = ddply(mp, .(Item, QUD, Alternatives, Quantifier, SpeakerOptimality,PriorExpectation_smoothed, WonkyWorldPrior, PosteriorExpectation_empirical), summarise, PosteriorExpectation_predicted=sum(State*PosteriorProbability)/15)
-head(pexpectations)
-some = droplevels(subset(pexpectations, Quantifier == "some"))
-
-cors = ddply(some, .(Alternatives, QUD, SpeakerOptimality, WonkyWorldPrior), summarise, r=cor(PosteriorExpectation_predicted, PosteriorExpectation_empirical))
-cors = cors[order(cors[,c("r")],decreasing=T),]
-head(cors)
-
-ggplot(some, aes(x=PosteriorExpectation_predicted, y=PosteriorExpectation_empirical,color=as.factor(SpeakerOptimality), shape=as.factor(WonkyWorldPrior))) +
-  geom_point() +
-  geom_smooth(method="lm") +
-  geom_abline(intercept=0,slope=1,color="gray50") +
-  scale_x_continuous(limits=c(0,1)) +
-  scale_y_continuous(limits=c(0,1)) +  
-#  geom_text(data=cors, aes(label=r)) +
-  #scale_size_discrete(range=c(1,2)) +
-  #scale_color_manual(values=c("red","blue","black")) +
-  facet_grid(QUD~Alternatives)
-ggsave("graphs/model-empirical-uniform-expectations.pdf",width=30,height=10)
-
-  
-#plot empirical against predicted allstate-prbabilities for "some"
-allstate = droplevels(subset(mp, State == 15 & Quantifier == "some"))
-cors = ddply(allstate, .(Alternatives, QUD, SpeakerOptimality, WonkyWorldPrior), summarise, r=cor(PosteriorProbability, PosteriorProbability_empirical))
-cors = cors[order(cors[,c("r")],decreasing=T),]
-head(cors)
-# .59 correlation despite being shitty model
-
-ggplot(allstate, aes(x=PosteriorProbability, y=PosteriorProbability_empirical,color=as.factor(WonkyWorldPrior), shape=as.factor(SpeakerOptimality))) +
-  geom_point() +
-  geom_smooth() +
-  geom_abline(intercept=0,slope=1,color="gray50") +
-  scale_x_continuous(limits=c(0,1)) +
-  scale_y_continuous(limits=c(0,1)) +  
-  #  geom_text(data=cors, aes(label=r)) +
-  #scale_size_discrete(range=c(1,2)) +
-  #scale_color_manual(values=c("red","blue","black")) +
-  facet_grid(QUD~Alternatives)
-ggsave("graphs/model-empirical-uniform-allstateprobs.pdf",width=30,height=10)
-
-#maybe COGSCI plot basis? plot  predicted allstate-prbabilities for "some" as a function of prior allstate-probabilities
-
-ggplot(allstate, aes(x=PriorProbability, y=PosteriorProbability,color=as.factor(WonkyWorldPrior), shape=as.factor(SpeakerOptimality))) +
-  geom_point() +
-  geom_smooth() +
-  geom_abline(intercept=0,slope=1,color="gray50") +
-  scale_x_continuous(limits=c(0,1)) +
-  scale_y_continuous(limits=c(0,1)) +  
-  #  geom_text(data=cors, aes(label=r)) +
-  #scale_size_discrete(range=c(1,2)) +
-  #scale_color_manual(values=c("red","blue","black")) +
-  facet_grid(QUD~Alternatives)
-ggsave("graphs/model-uniform-allstateprobs.pdf",width=30,height=10)
-
-# get empirical wonkiness posteriors
+############
+# empirical wonkiness posteriors
+############
 load("/Users/titlis/cogsci/projects/stanford/projects/thegricean_sinking-marbles/experiments/11_sinking-marbles-normal/results/data/r.RData")
 head(r)
 nrow(r)
@@ -450,56 +341,6 @@ ggplot(wonky_all, aes(x=PosteriorProbability, y=PosteriorProbability_empirical, 
   facet_grid(QUD~Alternatives)
 ggsave("graphs/model-empirical-uniform-wonkiness_all.pdf", width=30,height=10)
 
-ggplot(wonky_none, aes(x=PosteriorProbability, y=PosteriorProbability_empirical, color=as.factor(WonkyWorldPrior), shape=as.factor(SpeakerOptimality))) +
-  geom_point() +
-  geom_smooth(method="lm") +
-  geom_abline(intercept=0,slope=1,color="gray50") +
-  scale_x_continuous(limits=c(0,1)) +
-  scale_y_continuous(limits=c(0,1)) +  
-  facet_grid(QUD~Alternatives)
-ggsave("graphs/model-empirical-uniform-wonkiness_none.pdf", width=30,height=10)
-
-ggplot(wonky_some, aes(x=PosteriorProbability, y=PosteriorProbability_empirical, color=as.factor(WonkyWorldPrior), shape=as.factor(SpeakerOptimality))) +
-  geom_point() +
-  geom_smooth(method="lm") +
-  geom_abline(intercept=0,slope=1,color="gray50") +
-  scale_x_continuous(limits=c(0,1)) +
-  scale_y_continuous(limits=c(0,1)) +  
-  facet_grid(QUD~Alternatives)
-ggsave("graphs/model-empirical-uniform-wonkiness_some.pdf", width=30,height=10)
-
-
-
-ggplot(wonky_all, aes(x=PriorExpectation, y=PosteriorProbability, color=as.factor(WonkyWorldPrior), shape=as.factor(SpeakerOptimality))) +
-  geom_point() +
-  geom_smooth() +
-  scale_y_continuous(limits=c(0,1)) +  
-  facet_grid(QUD~Alternatives)  
-ggsave("graphs/model-uniform-all-wonkiness.pdf", width=30,height=10)
-
-ggplot(wonky_none, aes(x=PriorExpectation, y=PosteriorProbability, color=as.factor(WonkyWorldPrior), shape=as.factor(SpeakerOptimality))) +
-  geom_point() +
-  geom_smooth() +
-  scale_y_continuous(limits=c(0,1)) +  
-  facet_grid(QUD~Alternatives)  
-ggsave("graphs/model-uniform-none-wonkiness.pdf", width=30,height=10)
-
-ggplot(wonky_some, aes(x=PriorExpectation, y=PosteriorProbability, color=as.factor(WonkyWorldPrior), shape=as.factor(SpeakerOptimality))) +
-  geom_point() +
-  geom_smooth() +
-  scale_y_continuous(limits=c(0,1)) +  
-  facet_grid(QUD~Alternatives)  
-ggsave("graphs/model-uniform-some-wonkiness.pdf", width=30,height=10)
-
-save(mp, file="data/mp-uniform.RData")
-save(wr, file="data/wr-uniform.RData")
-
-wonky[wonky$Quantifier == "all" & wonky$PriorExpectation < 4 & wonky$Alternatives == "0_basic" & wonky$QUD == "how-many" & wonky$SpeakerOptimality == 2,]
-
-# get model predictions for all-state with basic alts, spopt==2, and qud==how-many
-mp_some_allstate = subset(mp, Alternatives=="0_basic" & SpeakerOptimality == 2 & QUD == "how-many" & State == 15)
-head(mp_some_allstate)
-nrow(mp_some_allstate)
 
 
 
