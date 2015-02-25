45afb4def2c7b1d8366adce6daeac707708ada5f
thegricean <judithdegen@gmail.com>
2015-02-24 18:29:17 -0800

    added results files

---
+++ experiments/19_speaker_reliability_withins/round1/approve.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DIR="/Users/titlis/cogsci/projects/stanford/projects/thegricean_sinking-marbles/experiments/19_speaker_reliability_withins/round1/"
+
+cd $MTURK_CMD_HOME/bin
+
+./approveWork.sh -successfile ${DIR}/sinking_marbles.success
