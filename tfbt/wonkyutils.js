b84ff342b308cf22913cdb5f1e532f075bf35b8a
mhtess <mht137@gmail.com>
2015-06-19 12:42:39 -0700

    first draft of tfbt; need to readin data, check factoring

---
+++ tfbt/wonkyutils.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+// var csvtojson = require('csvtojson');
+var babyparse = require('babyparse');
+
+function readCSV(filename){
+  return babyparse.parse(fs.readFileSync(filename, 'utf8'));
+};
+
+function writeCSV(jsonCSV, filename){
+  fs.writeFileSync(filename, babyparse.unparse(jsonCSV) + "\n");
+};
+
+
+function fillArray(value, len) {
+  var arr = [];
+  for (var i = 0; i < len; i++) {
+    arr.push(value);
+  }
+  return arr;
+};
+
+
+module.exports = {
+  readCSV: readCSV,
+  writeCSV: writeCSV,
+  fillArray: fillArray
+};
