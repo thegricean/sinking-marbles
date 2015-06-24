eff7aa2b20bfb58c06c7eaf5b7d224bd9c4eee8c
mhtess <mht137@gmail.com>
2015-06-23 22:59:32 -0700

    make marginal posteriors for parameters

--- bayesian_model_comparison/results/rscripts/processModelComparison.R
+++ bayesian_model_comparison/results/rscripts/processModelComparison.R
@@ -1,12 +1,40 @@
 library(tidyr)
 
 setwd("/Users/titlis/cogsci/projects/stanford/projects/thegricean_sinking-marbles/bayesian_model_comparison/results/")
+#setwd("~/Documents/research/sinking-marbles/bayesian_model_comparison/results")
 #source("../rscripts/helpers.r")
 
 d = read.csv("munged.csv",sep=",",quote="")
 head(d)
 summary(d)
 
-ggplot(d, aes(SpeakerOptimality)) +
-  geom_histogram() +
-  facet_grid(PosteriorProbability~Measure)
+# speaker optimality
+d.speakOpt <-unique(d %>% 
+  select(SpeakerOptimality, PosteriorProbability)) %>%
+  group_by(SpeakerOptimality) %>%
+  summarise(prob = sum(PosteriorProbability))
+
+ggplot(d.speakOpt, aes(x=SpeakerOptimality, y=prob)) +
+  geom_histogram(stat='identity', position=position_dodge())
+
+# wonkiness prior
+d.wonkiness <-unique(d %>% 
+                      select(WonkinessPrior, PosteriorProbability)) %>%
+  group_by(WonkinessPrior) %>%
+  summarise(prob = sum(PosteriorProbability))
+
+ggplot(d.wonkiness, aes(x=WonkinessPrior, y=prob)) +
+  geom_histogram(stat='identity', position=position_dodge())
+
+
+# LinkingBetaConcentration prior
+d.lbc <-unique(d %>% 
+          select(LinkingBetaConcentration, 
+                 PosteriorProbability)) %>%
+  group_by(LinkingBetaConcentration) %>%
+  summarise(prob = sum(PosteriorProbability))
+
+ggplot(d.lbc, aes(x=LinkingBetaConcentration, y=prob)) +
+  geom_histogram(stat='identity', position=position_dodge())
+
+
