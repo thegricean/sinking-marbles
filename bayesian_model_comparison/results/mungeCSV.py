49f965befa517e5d594f8fce5dd68e1a5476d857
thegricean <judithdegen@gmail.com>
2015-06-25 14:29:32 -0700

    munge updates

--- bayesian_model_comparison/results/mungeCSV.py
+++ bayesian_model_comparison/results/mungeCSV.py
@@ -4,8 +4,8 @@ f = open(sys.argv[1])
 lines = [l.rstrip() for l in f.readlines()]
 f.close()
 
-mungetype = sys.argv[2] # can be regular, 3speakers, wonkysoftmax
-headers = {"regular":["SpeakerOptimality","WonkinessPrior","LinkingBetaConcentration","PosteriorProbability","Measure","Item","Quantifier","Response","Probability"],"3speakers":["SpeakerOptimality1","SpeakerOptimality2","SpeakerOptimality3","WonkinessPrior","LinkingBetaConcentration","PosteriorProbability","Measure","Item","Quantifier","Response","Probability"],"wonkysoftmax":["SpeakerOptimality","WonkinessPrior","LinkingBetaConcentration","WonkySoftmax","PosteriorProbability","Measure","Item","Quantifier","Response","Probability"]}
+mungetype = sys.argv[2] # can be regular, 3speakers, wonkysoftmax, 3sp-ws
+headers = {"regular":["SpeakerOptimality","WonkinessPrior","LinkingBetaConcentration","PosteriorProbability","Measure","Item","Quantifier","Response","Probability"],"3speakers":["SpeakerOptimality1","SpeakerOptimality2","SpeakerOptimality3","WonkinessPrior","LinkingBetaConcentration","PosteriorProbability","Measure","Item","Quantifier","Response","Probability"],"wonkysoftmax":["SpeakerOptimality","WonkinessPrior","LinkingBetaConcentration","WonkySoftmax","PosteriorProbability","Measure","Item","Quantifier","Response","Probability"],"3sp-ws":["SpeakerOptimality1","SpeakerOptimality2","SpeakerOptimality3","WonkinessPrior","LinkingBetaConcentration","WonkySoftmax","PosteriorProbability","Measure","Item","Quantifier","Response","Probability"]}
 
 outfile = open("munged_"+mungetype+".csv","w")
 outfile.write(",".join(headers[mungetype]))
@@ -55,6 +55,22 @@ for l in lines[1:]:
 			probability = splited.pop(0)
 			outfile.write(",".join([speakerOptimality, wonkinessPrior,linkingBetaConcentration,wonkysoftmax,posteriorProb,measure,item,quantifier,response,probability]))
 			outfile.write("\n")
+	elif mungetype == "3sp-ws":
+		speakerOptimality1 = splited.pop(0).strip("\"")
+		speakerOptimality2 = splited.pop(0)
+		speakerOptimality3 = splited.pop(0)		
+		wonkinessPrior = splited.pop(0)		
+		linkingBetaConcentration = splited.pop(0)
+		wonkysoftmax = splited.pop(0)				
+		posteriorProb = splited.pop()		
+		while len(splited) > 0:
+			measure = splited.pop(0)
+			item = splited.pop(0)
+			quantifier = splited.pop(0)
+			response = splited.pop(0)
+			probability = splited.pop(0)
+			outfile.write(",".join([speakerOptimality1,speakerOptimality2,speakerOptimality3, wonkinessPrior,linkingBetaConcentration,wonkysoftmax,posteriorProb,measure,item,quantifier,response,probability]))
+			outfile.write("\n")			
 	else:
 		print "something's gone wrong"
 			
--- bayesian_model_comparison/results/rscripts/processModelComparison.R
+++ bayesian_model_comparison/results/rscripts/processModelComparison.R
@@ -153,3 +153,75 @@ ggplot(d.lbc, aes(x=LinkingBetaConcentration, y=prob)) +
   geom_histogram(stat='identity', position=position_dodge())
 ggsave("graphs/wonkysoftmax_linkbetaconcentration.png",width=8,height=6)
 
+
+### parse results for three spopt parameters and wonkiness softmax
+d = read.csv("munged_3sp-ws.csv",sep=",",quote="")
+nrow(d)
+head(d)
+summary(d)
+
+# since munged_xxx.csv has parameter values repeated for all items in posterior predictive
+# take only unique rows (unique sets of parameter values)
+d.params <- unique(d %>% select(SpeakerOptimality1, 
+                                SpeakerOptimality2,
+                                SpeakerOptimality3,
+                                WonkinessPrior,
+                                LinkingBetaConcentration,
+                                WonkySoftmax,
+                                PosteriorProbability))
+
+# speaker optimality 1
+d.speakOpt1 <- d.params %>% 
+  group_by(SpeakerOptimality1) %>%
+  summarise(prob = sum(PosteriorProbability))
+
+ggplot(d.speakOpt1, aes(x=SpeakerOptimality1, y=prob)) +
+  geom_histogram(stat='identity', position=position_dodge())
+ggsave("graphs/3sp-ws_compstate_spopt.png",width=8,height=6)
+
+# speaker optimality 2
+d.speakOpt2 <- d.params %>% 
+  group_by(SpeakerOptimality2) %>%
+  summarise(prob = sum(PosteriorProbability))
+
+ggplot(d.speakOpt2, aes(x=SpeakerOptimality2, y=prob)) +
+  geom_histogram(stat='identity', position=position_dodge())
+ggsave("graphs/3sp-ws_compallprob_spopt.png",width=8,height=6)
+
+# speaker optimality 3
+d.speakOpt3 <- d.params %>% 
+  group_by(SpeakerOptimality3) %>%
+  summarise(prob = sum(PosteriorProbability))
+
+ggplot(d.speakOpt3, aes(x=SpeakerOptimality3, y=prob)) +
+  geom_histogram(stat='identity', position=position_dodge())
+ggsave("graphs/3sp-ws_wonkiness_spopt.png",width=8,height=6)
+
+# wonkiness prior
+d.wonkiness <-d.params %>% 
+  group_by(WonkinessPrior) %>%
+  summarise(prob = sum(PosteriorProbability))
+
+ggplot(d.wonkiness, aes(x=WonkinessPrior, y=prob)) +
+  geom_histogram(stat='identity', position=position_dodge())
+ggsave("graphs/3sp-ws_wprior.png",width=8,height=6)
+
+# LinkingBetaConcentration prior
+d.lbc <- d.params %>% 
+  group_by(LinkingBetaConcentration) %>%
+  summarise(prob = sum(PosteriorProbability))
+
+ggplot(d.lbc, aes(x=LinkingBetaConcentration, y=prob)) +
+  geom_histogram(stat='identity', position=position_dodge(),width=.2)
+ggsave("graphs/3sp-ws_linkbetaconcentration.png",width=8,height=6)
+
+# wonkysoftmax 
+d.wsoftmax <-d.params %>% 
+  group_by(WonkySoftmax) %>%
+  summarise(prob = sum(PosteriorProbability))
+
+ggplot(d.wsoftmax, aes(x=WonkySoftmax, y=prob)) +
+  geom_histogram(stat='identity', position=position_dodge())
+ggsave("graphs/3sp-ws_wsoftmax.png",width=8,height=6)
+
+
