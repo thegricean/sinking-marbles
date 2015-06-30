c220a01b7dcb7432408eba167b891bc6a105923f
thegricean <judithdegen@gmail.com>
2015-06-29 19:57:14 -0700

    getting ready to run more people in wonky and allprob

--- bayesian_model_comparison/results/mungeCSVgeneral.py
+++ bayesian_model_comparison/results/mungeCSVgeneral.py
@@ -1,6 +1,6 @@
 # run with one of the following commands:
 # 
-# python mungeCSV.py wonky_tfbtPosterior_predictiveAnddiscreteParams_mh10000b1000.csv regula
+# python mungeCSV.py wonky_tfbtPosterior_predictiveAnddiscreteParams_mh10000b1000.csv regular
 #
 # python mungeCSV.py wonky_tfbtPosterior_predictiveAnddiscreteParams_3speakerOptimalities_mh10000b1000.csv 3speakers
 #
@@ -17,11 +17,12 @@ lines = [l.rstrip() for l in f.readlines()]
 f.close()
 
 mungetype = sys.argv[2] # can be regular, 3speakers, wonkysoftmax, 3sp-ws, 3sp-ws-2betas
-headers = [s.strip("\"") for s in lines[0].split(",")]
+headers = [s.strip("\"").title() for s in lines[0].split(",")]
 pp = headers.pop(-2)
 numheaders = len(headers)
 nheaders = headers + ["Measure","Item","Quantifier","Response","Probability"]
-print headers
+print numheaders
+print nheaders
 
 outfile = open("munged_"+mungetype+".csv","w")
 outfile.write(",".join(nheaders))
@@ -33,11 +34,17 @@ for l in lines[1:]:
 	for i in range(numheaders):
 		resultline.append(splited.pop(0).strip("\""))
 		resultline.append(splited.pop().strip("\""))
-		while len(splited) > 0:
-			resultline.append(splited.pop(0).strip("\""))		
-			outfile.write(",".join(resultline))
-			outfile.write("\n")
-				
+	print resultline
+	while len(splited) > 0:
+		toprintline = resultline	
+		measure = splited.pop(0).strip("\"")
+		item = splited.pop(0).strip("\"")
+		quantifier = splited.pop(0).strip("\"")
+		response = splited.pop(0).strip("\"")
+		probability = splited.pop(0).strip("\"")
+		outfile.write(",".join(toprintline+[measure,item,quantifier,response,probability]))
+		outfile.write("\n")	
+
 		
 outfile.close()		
 
